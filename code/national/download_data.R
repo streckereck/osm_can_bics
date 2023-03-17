2637a2099fee4c0075c07e06d9c87a84a71e7fef
streckereck <colin.ferster@gmail.com>
2023-03-16 22:01:21 -0700

    update to use Planet OSM

--- code/national/download_data.R
+++ code/national/download_data.R
@@ -17,49 +17,87 @@ source("./code/Can_BICS_OSM_classify.R")
 # OSM2PSQL style file for importing osm data
 style_path <- "./code/national/can_bics.style"
 
-# download from Geofabrik: http://download.geofabrik.de/north-america/canada.html
+# Switch from Geofabrik to Planet OSM
+# Download planet OSM file from: https://planet.osm.org/pbf/
+# Version 3 uses version 230123 to match Strava Metro
+
+# download stats can province boundary files from: https://www12.statcan.gc.ca/census-recensement/2021/geo/sip-pis/boundary-limites/index2021-eng.cfm?year=21
+# then open in QGIS, and for each province:
+# buffer out 1000m 
+# simplify with a threshold of 1000m
+# use the QGIS Export OSM Poly plugin to export, using the abbreviation in 
+# lowercase for a name (e.g. ab, bc, mb, etc.)
+
+# use osmosis to create a pbf for each province and territory:
+# osmosis --read-pbf file=planet-230123.osm.pbf --bounding-polygon file=nwt.poly --write-pbf nwt.pbf
+
+directory <- "C:/working/planet_osm/"
+
 provinces <- data.frame(
-  name = c("AB",
-          "BC",
-          "MB",
-          "NB",
-          "NF",
-          "NWT",
-          "NS",
-          "NV",
-          "ON",
-          "PEI",
-          "QC",
-          "SK",
-          "YT"),
-  filename = c("alberta-latest.osm.pbf",
-               "british-columbia-latest.osm.pbf",
-               "manitoba-latest.osm.pbf",
-               "new-brunswick-latest.osm.pbf",
-               "newfoundland-and-labrador-latest.osm.pbf",
-               "northwest-territories-latest.osm.pbf",
-               "nova-scotia-latest.osm.pbf",
-               "nunavut-latest.osm.pbf",
-               "ontario-latest.osm.pbf",
-               "prince-edward-island-latest.osm.pbf",
-               "quebec-latest.osm.pbf",
-               "saskatchewan-latest.osm.pbf",
-               "yukon-latest.osm.pbf"))
+  name = c("ab",
+           "bc",
+           "mb",
+           "nb",
+           "nf",
+           "nwt",
+           "ns",
+           "nvt",
+           "on_simple",
+           "pei",
+           "qc",
+           "sk",
+           "yt"))
 
-# testing: provinces <- provinces %>% filter(name %in% c("NWT"))
+provinces <- data.frame(provinces) 
+provinces$directory <- directory
 
-# download from geofabrik
-source = c("http://download.geofabrik.de/north-america/canada/")
+provinces$filename <- paste0(directory, provinces$name, ".pbf")
 
-for(i in 1:nrow(provinces)){
-  print(provinces$name[i])
-  print(provinces$filename[i])
-  download.file(url = paste0(source, provinces$filename[i]),
-                dest = paste0(national_data_local_directory, provinces$filename[i]),
-                method = "curl",
-                mode = "w")
+geofabrik <- F
+if(geofabrik){
+  # download from Geofabrik: http://download.geofabrik.de/north-america/canada.html
+  provinces <- data.frame(
+    name = c("AB",
+            "BC",
+            "MB",
+            "NB",
+            "NF",
+            "NWT",
+            "NS",
+            "NV",
+            "ON",
+            "PEI",
+            "QC",
+            "SK",
+            "YT"),
+    filename = c("alberta-latest.osm.pbf",
+                 "british-columbia-latest.osm.pbf",
+                 "manitoba-latest.osm.pbf",
+                 "new-brunswick-latest.osm.pbf",
+                 "newfoundland-and-labrador-latest.osm.pbf",
+                 "northwest-territories-latest.osm.pbf",
+                 "nova-scotia-latest.osm.pbf",
+                 "nunavut-latest.osm.pbf",
+                 "ontario-latest.osm.pbf",
+                 "prince-edward-island-latest.osm.pbf",
+                 "quebec-latest.osm.pbf",
+                 "saskatchewan-latest.osm.pbf",
+                 "yukon-latest.osm.pbf"))
+  
+  # testing: provinces <- provinces %>% filter(name %in% c("NWT"))
+  
+  # download from geofabrik
+  source = c("http://download.geofabrik.de/north-america/canada/")
+  
+  for(i in 1:nrow(provinces)){
+    print(provinces$name[i])
+    print(provinces$filename[i])
+    download.file(url = paste0(source, provinces$filename[i]),
+                  dest = paste0(national_data_local_directory, provinces$filename[i]),
+                  method = "curl",
+                  mode = "w")
+  }
 }
-
 # load into postGIS
 # this depends on having post gis installed, and a default account named 
 # postgres with no password (when executed in a local environment). 
@@ -87,7 +125,7 @@ for(i in 1:nrow(provinces)){
   system(paste0("osm2pgsql.exe -U postgres --slim -W -d ",
                 data_base_name,
                 " -S ", normalizePath(style_path), " ",
-                paste0(national_data_local_directory, provinces$filename[i])))
+                paste0(provinces$filename[i])))
 }
 
 # create a central database for all data
